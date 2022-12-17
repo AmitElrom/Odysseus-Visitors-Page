b47fb72f4ba73ee85eccfabc935126d808f92071
Amit Elrom <amitelrom99@gmail.com>
2022-12-17 10:36:54 +0200

    more on articles

--- src/components/pages/values and articles/articles/article/Article.jsx
+++ src/components/pages/values and articles/articles/article/Article.jsx
@@ -5,14 +5,16 @@ import classes from "./Article.module.css";
 const Article = ({ img, alt, title, title2, text }) => {
   return (
     <div className={classes.article}>
-      <img className={classes.img} src={img} alt={alt} />
+      <div className={classes["img-div"]}>
+        <img className={classes.img} src={img} alt={alt} />
+      </div>
       <div className={classes.data}>
         <h3>
           <span className={classes.span}>{title}</span>
           {title2 && <span className={classes.span}>{title2}</span>}
         </h3>
         <p>{text}</p>
-        <div>
+        <div className={classes["button-read-more-div"]}>
           <button>קראו עוד</button>
         </div>
       </div>
