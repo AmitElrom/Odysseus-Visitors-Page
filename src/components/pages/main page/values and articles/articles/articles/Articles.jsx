0038826865349ccca6c153510120361b4c1dd583
Amit Elrom <amitelrom99@gmail.com>
2022-12-23 15:49:34 +0200

    apply tech background

--- src/components/pages/main page/values and articles/articles/articles/Articles.jsx
+++ src/components/pages/main page/values and articles/articles/articles/Articles.jsx
@@ -4,11 +4,16 @@ import TheArticles from "../the articles/TheArticles";
 
 import classes from "./Articles.module.css";
 
+import techBgImg from "../../../../../../assets/values and articles/articles/tech-bg.png";
+
 const Articles = () => {
   return (
-    <div className={`blocks ${classes.articles}`}>
-      <Subtitle subtitle="מאמרים" id="מאמרים" />
-      <TheArticles />
+    <div className={classes.container}>
+      <div className={`blocks ${classes.articles}`}>
+        <Subtitle subtitle="מאמרים" id="מאמרים" />
+        <TheArticles />
+      </div>
+      <img className={classes["tech-bg"]} src={techBgImg} alt="tech bg" />
     </div>
   );
 };
--- src/components/pages/main page/values and articles/values/values/Values.jsx
+++ src/components/pages/main page/values and articles/values/values/Values.jsx
@@ -4,11 +4,16 @@ import TheValues from "../the values/TheValues";
 
 import classes from "./Values.module.css";
 
+import techBgImg from "../../../../../../assets/values and articles/values/tech-bg.png";
+
 const Values = () => {
   return (
-    <div className={`blocks ${classes.values}`}>
-      <Subtitle subtitle="ערכי הקרן" id="ערכי הקרן" />
-      <TheValues />
+    <div className={classes.container}>
+      <div className={`blocks ${classes.values}`}>
+        <Subtitle subtitle="ערכי הקרן" id="ערכי הקרן" />
+        <TheValues />
+      </div>
+      <img className={classes["tech-bg"]} src={techBgImg} alt="tech bg" />
     </div>
   );
 };
