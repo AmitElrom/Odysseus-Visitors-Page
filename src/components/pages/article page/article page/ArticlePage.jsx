2509329b87b6aecbf0aac1f3cb71a7e0bedcf130
Amit Elrom <amitelrom99@gmail.com>
2022-12-26 16:52:15 +0200

    on article page

--- src/components/pages/article page/article page/ArticlePage.jsx
+++ src/components/pages/article page/article page/ArticlePage.jsx
@@ -1,9 +1,15 @@
 import { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router";
+
 import { strapiApiContext } from "../../../../store/strapi-api-context";
+
+import Contact from "../../main page/contact/contact/Contact";
 import Paragraphs from "../paragraphs/paragraphs/Paragraphs";
 
 import classes from "./ArticlePage.module.css";
+
+import headerImg from "../../../../assets/article page/article_header.png";
+import Subtitle from "../../../UI/subtitle/Subtitle";
 
 const ArticlePage = () => {
   const [article, setArticle] = useState({
@@ -30,9 +36,12 @@ const ArticlePage = () => {
 
   return (
     <div className={classes.article}>
-      <h1>{article.title}</h1>
-      <Paragraphs paragraphs={article.paragraphs} />
-      <p className={classes.company}>קרן אודיסאוס – השקעה טכנולוגית</p>
+      <img className={classes["img-header"]} src={headerImg} alt="header" />
+      <div className={classes.data}>
+        <Subtitle subtitle={article.title} />
+        <Paragraphs paragraphs={article.paragraphs} />
+      </div>
+      <Contact />
     </div>
   );
 };
