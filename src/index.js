5c4813213c4bcc94e74fdebab94e1304288a5bc5
Amit Elrom <amitelrom99@gmail.com>
2023-01-02 16:27:13 +0200

    more fixes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import ScreenSizeContextProvider from "./store/screen-size-context";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
