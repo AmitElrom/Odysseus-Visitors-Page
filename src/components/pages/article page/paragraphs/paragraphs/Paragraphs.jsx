b72554a171b657003bfe46b0594a6e5027efbf00
Amit Elrom <amitelrom99@gmail.com>
2023-01-17 07:47:48 +0200

    fix style: article pars align to left in ltr

--- src/components/pages/article page/paragraphs/paragraphs/Paragraphs.jsx
+++ src/components/pages/article page/paragraphs/paragraphs/Paragraphs.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { useContext } from "react";
+import { sanityApiContext } from "../../../../../store/sanity-api-context";
 
 import Paragraph from "../paragraph/Paragraph";
 
 import classes from "./Paragraphs.module.css";
 
 const Paragraphs = ({ paragraphs }) => {
+  const { ltr } = useContext(sanityApiContext);
+
   const paragraphsList = paragraphs?.map((paragraph) => {
     return <Paragraph key={paragraph._key} {...paragraph} />;
   });
 
-  return <div className={classes.paragraphs}>{paragraphsList}</div>;
+  return (
+    <div
+      className={classes.paragraphs}
+      style={ltr ? { textAlign: "left" } : { textAlign: "right" }}
+    >
+      {paragraphsList}
+    </div>
+  );
 };
 
 export default Paragraphs;
